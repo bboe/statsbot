2a65e59e07012bc718a01c359acb1d627af78d0d
Bryce Boe <bbzbryce@gmail.com>
2016-07-31 14:55:08 -0700

    Add basic docopt support.

--- statsbot/cli.py
+++ statsbot/cli.py
@@ -1,8 +1,21 @@
-"""statsbot.cli module."""
+"""statsbot command line tool.
+
+Usage:
+  statsbot [options]
+
+Options:
+  -h --help  Output this message.
+  --version  Output statsbot's version string.
+
+"""
+from __future__ import print_function
+
+from docopt import docopt
+
 from .const import __version__
 
 
 def main():
     """Provide the entry point to the statsbot command."""
-    print('statsbot v{}'.format(__version__))
+    docopt(__doc__, version='statsbot v{}'.format(__version__))
     return 0

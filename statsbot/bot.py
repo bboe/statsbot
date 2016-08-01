ed9acb0a2d29d0a3f44c1f606b5b88d80db7ff6f
Bryce Boe <bbzbryce@gmail.com>
2016-07-31 22:38:17 -0700

    Parse request titles, and label and respond to invalid requests.

--- statsbot/bot.py
+++ statsbot/bot.py
@@ -1,5 +1,6 @@
 """statsbot.bot module."""
 import logging
+import re
 
 import praw.exceptions
 
@@ -16,8 +17,44 @@ class Bot(object):
 
     """
 
+    FLAIR_INVALID = 'UNSATISFIABLE'
     FLAIR_STATS = 'STATS'
     FLAIR_UNKNOWN = 'OTHER'
+
+    INVALID_MESSAGE = """Oops! Your request cannot be satisifed due to
+unrecognized syntax. Please ensure your title is of the following format and
+try again:
+
+    [request] SUBREDDIT_NAME VIEW
+
+Where `SUBREDDIT_NAME` is the name of the subreddit to run the stats on, and
+`VIEW` is either a __number__, or one of _all_, _day_, _hour_, _month_, _week_,
+or _year_. When `VIEW` is a __number__, it number represents the number of past
+days to collect submissions for using the _new_ listing. Otherwise, `VIEW`
+represents the respective _top_ listing.
+
+Examples:
+
+    [request] redditdev all
+    [request] subreddit_stats 730
+
+---
+
+If this message is in error please file a bug at
+https://github.com/bboe/statsbot/issues after using GitHub's search to ensure a
+similar issue does not already exist. Thanks!
+
+"""
+
+    RE_REQUEST = re.compile(r'^\[request\] (?:/?r/)?(?P<subreddit>\w+) '
+                            r'(?P<view>\d+|all|day|hour|month|week|year)$',
+                            re.IGNORECASE)
+
+    @classmethod
+    def parse_request_title(cls, title):
+        """Return the subreddit and view for the request."""
+        match = cls.RE_REQUEST.match(title)
+        return match.groupdict() if match else None
 
     def __init__(self, subreddit):
         """Initialize an instance of Bot.
@@ -25,6 +62,15 @@ class Bot(object):
         :param subreddit: The subreddit to monitor for new submissions.
         """
         self.subreddit = subreddit
+
+    def _handle_request(self, submission):
+        logger.debug('PARSING: {}'.format(submission.title))
+        permalink = self._permalink(submission)
+        params = self.parse_request_title(submission.title)
+        if params is None:
+            logger.info('INVALID: {}'.format(permalink))
+            self.subreddit.flair.set(submission, self.FLAIR_INVALID)
+            self._safe_reply(submission, self.INVALID_MESSAGE)
 
     def _handle_stats(self, submission):
         logger.info('STATS: {}'.format(self._permalink(submission)))
@@ -43,7 +89,7 @@ class Bot(object):
         logger.debug('FOUND: {}'.format(submission.title))
         lower_title = submission.title.lower()
         if lower_title.startswith('[request]'):
-            logger.info('REQUEST')
+            self._handle_request(submission)
         elif lower_title.startswith('subreddit stats:'):
             self._handle_stats(submission)
         else:
